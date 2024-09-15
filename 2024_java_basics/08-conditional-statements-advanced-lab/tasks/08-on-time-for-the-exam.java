268b6c2d67e973fee6658b5c68b58c34c13f4180
Stoyan Ivanov <115835362+stoyan-stunji@users.noreply.github.com>
2024-09-15 12:06:10 +0300

    Update 08-on-time-for-the-exam.java

--- 2024_java_basics/08-conditional-statements-advanced-lab/tasks/08-on-time-for-the-exam.java
+++ 2024_java_basics/08-conditional-statements-advanced-lab/tasks/08-on-time-for-the-exam.java
@@ -1,68 +1,50 @@
-using System;
+import java.util.Scanner;
 
-namespace _08_On_Time_for_the_Exam
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int hExam = int.Parse(Console.ReadLine());
-            int mExam = int.Parse(Console.ReadLine());
-            int hArrival = int.Parse(Console.ReadLine());
-            int mArrival = int.Parse(Console.ReadLine());
+public class OnTimeForTheExam {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            int mExamSum = (hExam * 60) + mExam;
-            int mArrivalSum = (hArrival * 60) + mArrival;
+        int hExam = scanner.nextInt();
+        int mExam = scanner.nextInt();
+        int hArrival = scanner.nextInt();
+        int mArrival = scanner.nextInt();
 
-            int difference = mArrivalSum - mExamSum;
+        int mExamSum = (hExam * 60) + mExam;
+        int mArrivalSum = (hArrival * 60) + mArrival;
 
-            if (difference > 0)
-            {
-                if (difference < 60)
-                {
-                    Console.WriteLine("Late");
-                    Console.WriteLine($"{difference} minutes after the start");
+        int difference = mArrivalSum - mExamSum;
+
+        if (difference > 0) {
+            if (difference < 60) {
+                System.out.println("Late");
+                System.out.println(difference + " minutes after the start");
+            } else {
+                int hours = difference / 60;
+                int minutes = difference % 60;
+                System.out.printf("Late%n%d:%02d hours after the start%n", hours, minutes);
+            }
+        } else if (difference == 0) {
+            System.out.println("On time");
+        } else {
+            difference = -difference; // Make it positive for easier handling
+
+            if (difference <= 30) {
+                System.out.println("On time");
+                if (difference != 0) {
+                    System.out.println(difference + " minutes before the start");
                 }
-                else
-                {
+            } else {
+                if (difference < 60) {
+                    System.out.println("Early");
+                    System.out.println(difference + " minutes before the start");
+                } else {
                     int hours = difference / 60;
                     int minutes = difference % 60;
-                    Console.WriteLine("Late");
-                    Console.WriteLine($"{hours}:{minutes:D2} hours after the start");
-                }
-            }
-            else if (difference == 0)
-            {
-                Console.WriteLine("On time");
-            }
-            else
-            {
-                difference = -difference; // Make it positive for easier handling
-
-                if (difference <= 30)
-                {
-                    Console.WriteLine("On time");
-                    if (difference != 0)
-                    {
-                        Console.WriteLine($"{difference} minutes before the start");
-                    }
-                }
-                else
-                {
-                    if (difference < 60)
-                    {
-                        Console.WriteLine("Early");
-                        Console.WriteLine($"{difference} minutes before the start");
-                    }
-                    else
-                    {
-                        int hours = difference / 60;
-                        int minutes = difference % 60;
-                        Console.WriteLine("Early");
-                        Console.WriteLine($"{hours}:{minutes:D2} hours before the start");
-                    }
+                    System.out.printf("Early%n%d:%02d hours before the start%n", hours, minutes);
                 }
             }
         }
+
+        scanner.close();
     }
 }
