15f97f1a42b933fa4f01b5c742228d484865d54b
Stoyan Ivanov <115835362+stoyan-stunji@users.noreply.github.com>
2024-09-15 11:56:51 +0300

    Create 02-summer-outfit.java

---
+++ 2024_java_basics/08-conditional-statements-advanced-lab/tasks/02-summer-outfit.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class SummerOutfit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double degrees = Double.parseDouble(scanner.nextLine());
+        String typeOfDay = scanner.nextLine();
+        String outfit = "";
+        String shoes = "";
+
+        if (degrees >= 10 && degrees <= 18) {
+            switch (typeOfDay) {
+                case "Morning":
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                    break;
+                case "Afternoon":
+                case "Evening":
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                    break;
+            }
+        } else if (degrees > 18 && degrees <= 24) {
+            switch (typeOfDay) {
+                case "Morning":
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                    break;
+                case "Afternoon":
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                    break;
+                case "Evening":
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                    break;
+            }
+        } else if (degrees >= 25) {
+            switch (typeOfDay) {
+                case "Morning":
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                    break;
+                case "Afternoon":
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                    break;
+                case "Evening":
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                    break;
+            }
+        }
+
+        System.out.printf("It's %.1f degrees, get your %s and %s.%n", degrees, outfit, shoes);
+        scanner.close();
+    }
+}
