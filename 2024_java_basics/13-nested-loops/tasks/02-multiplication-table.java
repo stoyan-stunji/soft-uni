d50ae01757665aa8fbcf26604a4ee28cd1f60076
Stoyan Ivanov <115835362+stoyan-stunji@users.noreply.github.com>
2024-09-15 13:23:57 +0300

    Update and rename New Text Document.txt to 02-multiplication-table.java

---
+++ 2024_java_basics/13-nested-loops/tasks/02-multiplication-table.java
@@ -0,0 +1,9 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for (int n1 = 1; n1 <= 10; n1++) {
+            for (int n2 = 1; n2 <= 10; n2++) {
+                System.out.printf("%d * %d = %d%n", n1, n2, n1 * n2);
+            }
+        }
+    }
+}
