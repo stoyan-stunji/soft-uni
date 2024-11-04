2be6bdcd13f19d7b59b582b8cb75832524faff7f
Stoyan Ivanov <115835362+stoyan-stunji@users.noreply.github.com>
2024-11-04 22:26:34 +0200

    Add files via upload

---
+++ 2024_java_basics/03-first-steps-in-programing/Demo.java
@@ -0,0 +1,59 @@
+package FirstStepsInCoding;
+
+import java.util.Scanner;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        /*System.out.println("Hello World!");
+
+        String firstName = scanner.nextLine();
+        System.out.println(firstName);
+
+        String lastName = scanner.nextLine();
+        System.out.println(lastName);
+
+        int age = Integer.parseInt(scanner.nextLine());
+        System.out.println(age);
+
+        char firstLetterOfFirstName = scanner.nextLine().charAt(0);
+        System.out.println(firstLetterOfFirstName);
+
+        double grade = Double.parseDouble(scanner.nextLine());
+        System.out.println(grade);*/
+
+        //1. Събиране +
+        //2. Изваждане -
+        //3. Умножение *
+        //4. Деление /
+        //5. Деление с остатък % -> като резултат получаваме остатъка от целочисленото деление
+
+        /*int num1 = 10;
+        int num2 = 3;
+
+        System.out.println(num1/num2);//3
+        System.out.println(num1 % num2);//1*/
+
+        String  name = "Yuli";
+
+        System.out.println("Hello, " + name + "!");
+
+        System.out.println("Hello: " + (7 + 10));
+
+        //Шаблони
+        // %s -> String
+        // %d -> int
+        // %f -> double
+        // %c -> char
+        // %n -> new line
+
+
+
+
+
+    }
+
+}
