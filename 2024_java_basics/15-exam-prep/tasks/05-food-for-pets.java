eca16da583e0811e576006cdb06c9f1791d81f39
Stoyan Ivanov <115835362+stoyan-stunji@users.noreply.github.com>
2024-12-13 22:24:12 +0200

    Update 05-food-for-pets.java

--- 2024_java_basics/15-exam-prep/tasks/05-food-for-pets.java
+++ 2024_java_basics/15-exam-prep/tasks/05-food-for-pets.java
@@ -1,41 +1,68 @@
-package ExamPrep;
 
 import java.util.Scanner;
+import java.util.Vector;
 
-public class FoodForPets_04 {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int days = Integer.parseInt(scanner.nextLine());
-        double totalFood = Double.parseDouble(scanner.nextLine());
+        Integer days = Integer.parseInt(scanner.nextLine());
+        Double food = Double.parseDouble(scanner.nextLine());
 
-        double dogEatenFood = 0;
-        double catEatenFood = 0;
-        double allBiscuitsInGr = 0;
-        for (int day = 1; day <= days; day++) {
+        Vector<Integer> dogFood = new Vector<>();
+        Vector<Integer> catFood = new Vector<>();
 
-            double dogFood = Double.parseDouble(scanner.nextLine());
-            double catFood = Double.parseDouble(scanner.nextLine());
+        Double cookies = 0.0;
 
-            dogEatenFood = dogEatenFood + dogFood;
-            catEatenFood = catEatenFood + catFood;
+        for(int i = 1; i <= days; ++i)
+        {
+            Integer j = Integer.parseInt(scanner.nextLine());
+            dogFood.add(j);
 
-            if (day % 3 == 0) {
-                double bonusFood = (dogFood + catFood) * 0.10;
-                allBiscuitsInGr = allBiscuitsInGr + bonusFood;
+            Integer k = Integer.parseInt(scanner.nextLine());
+            catFood.add(k);
+
+            if (i % 3 == 0)
+            {
+                double bonus = (j + k) * 0.10;
+                cookies = cookies + bonus;
             }
-
         }
 
-        double totalEatenFood = dogEatenFood + catEatenFood;
-        double percentageEatenFood = totalEatenFood / totalFood * 100;
-        double percentageDog = dogEatenFood / totalEatenFood * 100;
-        double percentageCat = catEatenFood / totalEatenFood * 100;
+        Integer allFood = 0;
+        for(int i = 0; i < dogFood.size(); ++i)
+        {
+            allFood += dogFood.get(i);
+        }
+        for(int i = 0; i < catFood.size(); ++i)
+        {
+            allFood += catFood.get(i);
+        }
 
+        Double allFoodPercent = (allFood / food) * 100;
+        // System.out.println(allFoodPercent);
 
-        System.out.printf("Total eaten biscuits: %dgr.%n", Math.round(allBiscuitsInGr));
-        System.out.printf("%.2f%% of the food has been eaten.%n", percentageEatenFood);
-        System.out.printf("%.2f%% eaten from the dog.%n", percentageDog);
-        System.out.printf("%.2f%% eaten from the cat.%n", percentageCat);
+        Double onlyDogFood = 0.0;
+        for(int i = 0; i < dogFood.size(); ++i)
+        {
+            onlyDogFood += dogFood.get(i);
+        }
+
+        Double dogFoodPercent = (onlyDogFood / allFood) * 100;
+        // System.out.println(dogFoodPercent);
+
+        Double onlyCatFood = 0.0;
+        for(int i = 0; i < catFood.size(); ++i)
+        {
+            onlyCatFood += catFood.get(i);
+        }
+
+        Double catFoodPercent = (onlyCatFood / allFood) * 100;
+        // System.out.println(catFoodPercent);
+
+        System.out.printf("Total eaten biscuits: %dgr.%n", Math.round(cookies));
+        System.out.printf("%.2f%% of the food has been eaten.%n", allFoodPercent);
+        System.out.printf("%.2f%% eaten from the dog.%n", dogFoodPercent);
+        System.out.printf("%.2f%% eaten from the cat.%n", catFoodPercent);
     }
 }
